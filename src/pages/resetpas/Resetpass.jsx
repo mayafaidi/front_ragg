2698e706cad0fdff2ee8df81bf15e0bf4686a679
Maya faidi <mayafaidi20@gmail.com>
2025-09-30 12:46:16 -0700

    ubdate some issue3

--- src/pages/resetpas/Resetpass.jsx
+++ src/pages/resetpas/Resetpass.jsx
@@ -23,7 +23,11 @@ const schema = yup.object({
   newPassword: yup
     .string()
     .required("New password is required")
-    .min(6, "Password must be at least 6 characters"),
+     .min(6, "Min length is 6")
+    .matches(/[A-Z]/, "Must contain at least one uppercase letter")
+    .matches(/[a-z]/, "Must contain at least one lowercase letter")
+    .matches(/[0-9]/, "Must contain at least one number")
+    .matches(/[@$!%*?&]/, "Must contain at least one special character"),
   confirmPassword: yup
     .string()
     .oneOf([yup.ref("newPassword"), null], "Passwords must match")
